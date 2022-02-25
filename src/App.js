93b1da93d475c71f5c0e50d176808a37debd4e49
Jownsu <jhonesdigno777@gmail.com>
2022-02-25 16:03:27 +0800

    migrated with github rest api

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 //pages
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
-  return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <NavBar />
+    return (
+        <GithubProvider>
+            <AlertProvider>
+                <Router>
+                    <div className="flex flex-col justify-between h-screen">
+                        <NavBar />
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
+                        <main className="container mx-auto px-3 pb-12">
+                            <Alert />
 
-        <Footer />
-      </div>
-    </Router>
-  )
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route
+                                    path="/notfound"
+                                    element={<NotFound />}
+                                />
+                                <Route path="/*" element={<NotFound />} />
+                            </Routes>
+                        </main>
+
+                        <Footer />
+                    </div>
+                </Router>
+            </AlertProvider>
+        </GithubProvider>
+    )
 }
 
 export default App
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,51 @@
+import React, { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({ children }) => {
+    const initialState = {
+        users: [],
+        loading: false,
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    const searchUsers = async (text) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/search/users?q=${text}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        const { items } = await response.json()
+        dispatch({
+            type: 'GET_USERS',
+            payload: items,
+        })
+    }
+
+    const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
+    return (
+        <GithubContext.Provider
+            value={{
+                users: state.users,
+                loading: state.loading,
+                searchUsers,
+                clearUsers,
+            }}
+        >
+            {children}
+        </GithubContext.Provider>
+    )
+}
+
+export default GithubContext
